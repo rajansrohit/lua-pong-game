cae6297bfc96259b57170c3da691a13f8c66fbfa
Rohit Rajan <41314110+rajansrohit@users.noreply.github.com>
2020-04-30 17:36:08 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -213,11 +213,4 @@ function love.draw()
     push:finish()
 end
 
-function displayScore()
-    love.graphics.setFont(scoreFont)
-    love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50,
-        VIRTUAL_HEIGHT / 3)
-    love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 30,
-        VIRTUAL_HEIGHT / 3)
-end
 
